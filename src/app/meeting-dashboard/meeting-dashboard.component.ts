ae32ccb4e5d93cf38a22d381e6dbf15cf07c3c2e
Ronette Guevara <ronetteguevara@gmail.com>
2024-02-07 19:50:44 -0800

    Ongoing development for Angular Project 1

---
+++ src/app/meeting-dashboard/meeting-dashboard.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-meeting-dashboard',
+  templateUrl: './meeting-dashboard.component.html',
+  styleUrl: './meeting-dashboard.component.css'
+})
+export class MeetingDashboardComponent {
+
+}

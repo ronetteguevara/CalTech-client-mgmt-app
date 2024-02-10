a2974dd1568254fdb9b9d72960666b077ef13354
Ronette Guevara <ronetteguevara@gmail.com>
2024-02-09 20:54:25 -0800

    First course-end project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { CreateMeetingComponent } from './create-meeting/create-meeting.component';
 import { MeetingDashboardComponent } from './meeting-dashboard/meeting-dashboard.component';
+import { MeetingsComponent } from './meetings/meetings.component';
+import { ClientsComponent } from './clients/clients.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CreateMeetingComponent,
-    MeetingDashboardComponent
+    MeetingDashboardComponent,
+    MeetingsComponent,
+    ClientsComponent
   ],
   imports: [
     BrowserModule,
